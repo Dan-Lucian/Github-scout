471dc16a0ea1a0fcb1b498878a9c9ed5ef748c02
Dan-Lucian <danugaina1403@gmail.com>
2021-09-17 22:42:05 +0300

    almost done with data display

--- js/set-search-btn-event.js
+++ js/set-search-btn-event.js
@@ -9,29 +9,34 @@ async function updateUser() {
   const inputValue = input.value;
   input.value = '';
 
-  const userData = await fetchUserData(inputValue);
-  setUserData(userData);
-
-  // const profileImg = document.getElementById('profile-img');
-  // const userName = document.getElementById('user-name');
-  // const profileLink = document.getElementById('profile-link');
-  // const dateJoined = document.getElementById('date-joined');
-  // const bio = document.getElementById('bio');
-
-  console.log(userData);
+  try {
+    const userData = await fetchUserData(inputValue);
+    setUserData(userData);
+    console.log(userData);
+  } catch (err) {
+    console.log('ooops an error occured during data fetching');
+    console.log('error message: ' + err.message);
+  }
 }
 
 async function fetchUserData(userName) {
   const response = await fetch(`https://api.github.com/users/${userName}`);
-  const data = response.json();
+  const data = await response.json();
   return data;
 }
 
 function setUserData(userData) {
   const infoElements = document.querySelectorAll('[data-user]');
-  console.log(infoElements);
 
-  infoElements.forEach(element => {
-    element.innerHTML = userData[element.dataset.user];
+  infoElements.forEach((element) => {
+    element.innerHTML = userData[element.dataset.user] || 'not provided';
+
+    //fix number 0 triggering 'not provided'
+    if (userData[element.dataset.user] === 0) element.innerHTML = 0;
+
+    // handle complex individual cases
+    if (element.dataset.user === 'login') element.href = userData['html_url'];
+    if (element.dataset.user === 'avatar_url')
+      element.src = userData['avatar_url'];
   });
 }
